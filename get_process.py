82863967ee36a3321a346f2b9cffa8d8c172945a
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-06-17 18:37:57 -0300

    feat: add call pwsh function

--- get_process.py
+++ get_process.py
@@ -1,4 +1,9 @@
+from scripts.call_script import call_pwsh
+
+def get_syspax():
+    #chama script para verificar se existe algum input pendente
+    call_pwsh('Syspax_DownloadFiles_Api')
 
 
-def get():
-    ...
+if __name__ == "__main__":
+    get_syspax()
