43ffc7e658b49c8cf7cff11a30e8ea341e68f323
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-06-17 20:32:07 -0300

    feat: Creating a query for pending files and updating them in Syspax.

--- get_process.py
+++ get_process.py
@@ -1,9 +1,20 @@
 from scripts.call_script import call_pwsh
+from database.queries import select_arquivos
 
 def get_syspax():
     #chama script para verificar se existe algum input pendente
-    call_pwsh('Syspax_DownloadFiles_Api')
+    # call_pwsh('Syspax_DownloadFiles_Api.ps1')
 
+    #realizas select em input arquivos procurando status 'Novo'
+    rows = select_arquivos('Novso')
+
+    #Loop sobre dados retornados
+    for row in rows:
+        #Seta ID syspax
+        id_syspax = row[0]
+        
+        #Chama script para alterar status no syspax
+        call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Em Fila"')
 
 if __name__ == "__main__":
     get_syspax()
