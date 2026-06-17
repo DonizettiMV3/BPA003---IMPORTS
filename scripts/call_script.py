0bab28591bcfbd079f2e8af6d9ae5bf736c401f4
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-06-17 16:04:58 -0300

    feat: create call function

---
+++ scripts/call_script.py
@@ -0,0 +1,10 @@
+import subprocess
+
+ROOT = 'C:\\MV3\BPA003 - PAX Capital - SYSPAX\\03. Scripts Powershell\\INPUT_MANUAL'
+
+def call_pwsh(file:str, param1:str = '', param2:str = ''):
+    subprocess.run([
+        'powershell',
+        '-File',
+        f'{ROOT}\\{file} "{param1}" "{param2}"'
+    ])
