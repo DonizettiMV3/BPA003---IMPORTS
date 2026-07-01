e26f73987b233bbb3f6a74b989c2b3473edf1f3e
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-07-01 11:19:37 -0300

    feat: Import validations completed.

--- import_process.py
+++ import_process.py
@@ -40,9 +40,23 @@ for row in rows:
         #Realiza update em processos já existentes da dados_processos
         db.imports_updateDuplicateProcess(date_now, file_name)
 
+        #Realiza update em processos com nome de requerente maior que 255
+        sh.update_process(condition=f"LENGTH(nome_requerente)>255 AND nome_arquivo_importacao ='{file_name}'", 
+        set= f"status ='Não importado', execucao_importacao='Número de caracteres para nome de requerente acima do limite de API', data_importacao = '{date_now}'")
 
+        #Realiza update em processos com campo faltando
+        sh.update_process(condition=f""" ((data_autuacao IS NULL OR LENGTH(data_autuacao)<=0)
+        OR (valor_espelho_principal IS NULL OR LENGTH(valor_espelho_principal)<=0)
+        OR (nome_requerente IS NULL OR LENGTH(nome_requerente)<=0)
+        OR (numero_processo IS NULL OR LENGTH(numero_processo)<=0)) 
+        AND caminho_arquivo_importacao ='{file_name}' AND status='Lemitti - Pendente'""",
+        set= f"status ='Não importado', execucao_importacao='Identificado campo(s) necessário(s) vazio(s)', data_importacao = '{date_now}'")
 
+        #Realiza update de arquivo para Lemmit - Pendente
+        sh.update_arquivo(id= id_syspax,
+                          set= f"status_processamento = 'Lemitti - Pendente', data_importacao='{date_now}'")
 
-
+    #Chama script para atualizar status no syspax
+    call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Processando"')
 
 
