2bef6f9efa90379531186fcb60591579654bb10f
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-07-01 10:05:35 -0300

    feat: create validation import temp

--- import_process.py
+++ import_process.py
@@ -1,5 +1,6 @@
 from scripts.call_script import call_pwsh
-from database.queries import select_arquivos
+from database.queries import select_arquivos,imports_deleteTemp, imports_selectTemp, update_arquivo
+from datetime import datetime
 
 rows = select_arquivos('Novo')
 
@@ -8,11 +9,31 @@ for row in rows:
     id = row[0]
     id_syspax = row[1]
     file_name = row[2]
-    path_name = row[3]
+    file_path = row[3]
+    date_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     #Chama script para mudar status no syspax
     call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Validando Dados"')
 
-    
+    #Deleta tabela de input temp
+    imports_deleteTemp()
+
+    #Executa scripts para imports
+    call_pwsh('Import_File.ps1', f'"{file_path}"')
+
+    #Verifica se linhas foram inseridas
+    temp_verify = imports_selectTemp()
+
+    count = 0
+    count = temp_verify[0]
+
+    if count <= 0:
+        #Dados não encontrados, setando erro no arquivo
+        update_arquivo(id_syspax, f"status_processamento = 'Erro', data_importacao='{date_now}'")
+
+    else:
+
+        ...
+
 
 
