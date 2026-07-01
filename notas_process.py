003b61dd7d4edc67d6202345769b920fb45da3cd
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-07-01 14:47:59 -0300

    refactor: create notas function

--- notas_process.py
+++ notas_process.py
@@ -2,25 +2,27 @@ from scripts.call_script import call_pwsh
 import database.queries.shared as sh
 from datetime import datetime
 
-rows = sh.select_arquivos('Syspax - Processado')
+def notas():
 
-for row in rows:
-    #Setando variaveis
-    id = row[0]
-    id_syspax = row[1]
-    file_name = row[2]
-    date_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    rows = sh.select_arquivos('Syspax - Processado')
 
-    #Atualiza arquivo no sysoax
-    call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Cadastrando Notas - Syspax"')
+    for row in rows:
+        #Setando variaveis
+        id = row[0]
+        id_syspax = row[1]
+        file_name = row[2]
+        date_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-    #Executa script para Notas
-    call_pwsh('Syspax_Notas_Api.ps1', f'"{file_name}"')
+        #Atualiza arquivo no sysoax
+        call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Cadastrando Notas - Syspax"')
 
-    #Atualiza arquivo para telefones
-    sh.update_arquivo(id = id,
-                          set =  f"""status_processamento = 'Telefone', fim_cadastro_notas='{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',  inicio_cadastro_notas = '{date_now}'""")
-    
-    #Atualiza arquivo no sysoax
-    call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Processando"')
+        #Executa script para Notas
+        call_pwsh('Syspax_Notas_Api.ps1', f'"{file_name}"')
+
+        #Atualiza arquivo para telefones
+        sh.update_arquivo(id = id,
+                            set =  f"""status_processamento = 'Telefone', fim_cadastro_notas='{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',  inicio_cadastro_notas = '{date_now}'""")
+        
+        #Atualiza arquivo no sysoax
+        call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Processando"')   
     
