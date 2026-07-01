b77299fdc194d336c3653fbe6a9b1ab3558e706d
Donizetti-MV3 <donizetti.santos@mv3.com.br>
2026-07-01 10:57:39 -0300

    feate: create validates process

--- import_process.py
+++ import_process.py
@@ -1,8 +1,9 @@
 from scripts.call_script import call_pwsh
-from database.queries import select_arquivos,imports_deleteTemp, imports_selectTemp, update_arquivo
+import database.queries.imports_query as db
+import database.queries.shared as sh
 from datetime import datetime
 
-rows = select_arquivos('Novo')
+rows = sh.select_arquivos('Novo')
 
 for row in rows:
     #Setando variaveis
@@ -16,24 +17,32 @@ for row in rows:
     call_pwsh('Syspax_UpdateStatus_Api.', f'"{id_syspax}"', f'"Validando Dados"')
 
     #Deleta tabela de input temp
-    imports_deleteTemp()
+    db.imports_deleteTemp()
 
     #Executa scripts para imports
     call_pwsh('Import_File.ps1', f'"{file_path}"')
 
     #Verifica se linhas foram inseridas
-    temp_verify = imports_selectTemp()
+    temp_verify = db.imports_selectTemp()
 
     count = 0
     count = temp_verify[0]
 
     if count <= 0:
         #Dados não encontrados, setando erro no arquivo
-        update_arquivo(id_syspax, f"status_processamento = 'Erro', data_importacao='{date_now}'")
+        db.update_arquivo(id_syspax, f"status_processamento = 'Erro', data_importacao='{date_now}'")
 
     else:
 
-        ...
+        #Dados localizados na temp, inserindo na tabela de PROD
+        db.imports_insertTemp(date_now, file_name, file_path)
+
+        #Realiza update em processos já existentes da dados_processos
+        db.imports_updateDuplicateProcess(date_now, file_name)
+
+
+
+
 
 
 
